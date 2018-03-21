84bde18f6ad4b83adeb07e1f453544a2d96b8a2b
Van Ha <hav206@yahoo.com>
2018-03-21 08:09:58 -0700

    week1

---
+++ loop.js
@@ -0,0 +1,36 @@
+for (var i = 0; i < 99; i++)
+{
+	console.log(i);
+}
+
+for ( var j = 99; j >0; j++)
+{
+	console.log(j);
+}
+
+for (var i = 0; i < 98; i++){
+	if(i % 2 == 0)
+	{
+		console.log(i);
+	}
+}
+
+for (var i = 98; i > 0; i--){
+	if(i % 2 == 0){
+		console.log(i)
+	}
+}
+
+for (var i = 49; i < 72; i++){
+	console.log(i);
+}
+
+for ( var i = 98; i >=26; i--)
+{
+	console.log(i);
+}
+
+for ( var i = 3; i < 90;i = i+3)
+{
+	console.log(i);
+}
